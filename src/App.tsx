0c1763fad8a87997c679f60736e0528fc65434e5
abdulrehmanwaseem <abdulrehman.code1@gmail.com>
2024-11-08 12:35:14 +0500

    Basic color picker add

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,32 @@
+import { useState } from "react";
+
 function App() {
+  const [color, setColor] = useState("");
+
   const onClick = async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      func: () => {
-        document.body.style.backgroundColor = "pink";
-      },
-    });
+    if (tab?.id !== undefined) {
+      chrome.scripting.executeScript<string[], void>({
+        target: { tabId: tab.id },
+        args: [color],
+        func: (color) => {
+          document.body.style.backgroundColor = color;
+        },
+      });
+    } else {
+      console.error("Active tab or tab ID not found.");
+      return <p className="text-error">Active tab or tab ID not found.</p>;
+    }
   };
 
   return (
     <>
-      <button onClick={onClick}>Click</button>
+      <input
+        type="color"
+        onChange={(e) => setColor(e.currentTarget.value)}
+        value={color}
+      />
+      <button onClick={onClick}>Change color</button>
     </>
   );
 }
